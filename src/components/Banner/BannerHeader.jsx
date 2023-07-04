dea71883a69c3ba69bd8315d59fae671ccad170c
huynhdev24 <huynh.dev.24@gmail.com>
2023-07-04 14:05:27 +0700

    add BannerHeader component

---
+++ src/components/Banner/BannerHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BannerHeader = () => {
+    return (
+        <section className="banner">
+            <img className="banner__img" src="./assets/images/banner.png" alt="banner" />
+        </section>
+    );
+};
+
+export default BannerHeader;
